f054befd33d33d98fbed7a202dd9d19580ae420c
JojoLindo-C24 <ra52702356842@acaonsfatima.org.br>
2021-11-05 15:56:44 -0300

    ...

--- react/tcc/src/pages/admEsqueceuSenha/index.js
+++ react/tcc/src/pages/admEsqueceuSenha/index.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 
 export default function EsqueceuSenhaAdm() {
     return(
-        <Container></Container>
+        <Container>
+            <Link></Link>
+        </Container>
     )
 }
