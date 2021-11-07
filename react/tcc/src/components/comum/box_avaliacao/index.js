b4d3217ae17192867f2bcfa04d4f5469ed5e3921
LucianeC <lucianecosta087@gmail.com>
2021-11-07 04:19:24 -0300

    scroll-admavliacao

--- react/tcc/src/components/comum/box_avaliacao/index.js
+++ react/tcc/src/components/comum/box_avaliacao/index.js
@@ -33,6 +33,41 @@ export default function Index(){
                     </div>
                     <div className="data">24/09/2021</div>
                  </div>
+                 <div className="box">
+                    <div className="box-mensagem">
+                        <div className= "nome">Joelma Marins do Santos</div>
+                        <div className="mensagem">Otimo atendimento, Parabéns!!</div>
+                    </div>
+                    <div className="data">24/09/2021</div>
+                 </div>
+                 <div className="box">
+                    <div className="box-mensagem">
+                        <div className= "nome">Joelma Marins do Santos</div>
+                        <div className="mensagem">Otimo atendimento, Parabéns!!</div>
+                    </div>
+                    <div className="data">24/09/2021</div>
+                 </div>
+                 <div className="box">
+                    <div className="box-mensagem">
+                        <div className= "nome">Joelma Marins do Santos</div>
+                        <div className="mensagem">Otimo atendimento, Parabéns!!</div>
+                    </div>
+                    <div className="data">24/09/2021</div>
+                 </div>
+                 <div className="box">
+                    <div className="box-mensagem">
+                        <div className= "nome">Joelma Marins do Santos</div>
+                        <div className="mensagem">Otimo atendimento, Parabéns!!</div>
+                    </div>
+                    <div className="data">24/09/2021</div>
+                 </div>
+                 <div className="box">
+                    <div className="box-mensagem">
+                        <div className= "nome">Joelma Marins do Santos</div>
+                        <div className="mensagem">Otimo atendimento, Parabéns!!</div>
+                    </div>
+                    <div className="data">24/09/2021</div>
+                 </div>
             </div>
         </BoxAvaliacao>
     )
--- react/tcc/src/components/comum/box_avaliacao/styled.js
+++ react/tcc/src/components/comum/box_avaliacao/styled.js
@@ -8,17 +8,42 @@ const BoxAvaliacao = styled.div`
     justify-content: center;
     width:78em;
     height: 26em;
+    overflow-y: auto;
     
+}
+
+.container::-webkit-scrollbar-track
+{
+	
+	background-color: #1C1C1C;
+	border-radius: 10px;
+}
+
+.container::-webkit-scrollbar
+{
+	width: 10px;
+	background-color: #000000;
+}
+
+.container::-webkit-scrollbar-thumb
+{
+	border-radius: 10px;
+	background-image: -webkit-gradient(linear,
+									   left bottom,
+									   left top,
+									   color-stop(0.44, rgb(129,153,227)),
+									   color-stop(0.72, rgb(73,125,189)),
+									   color-stop(0.96, rgb(28,58,148)));
 }
 
 .box{
     display: flex;
     padding: 1.4em 2.0em;
-   
+    
 }
 
 .box-mensagem{
-    
+   
 }
 
 .data{
